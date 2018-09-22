72461340e6c2af8f4fda187d50a28ef3d2687df3
Unknown <jasonl960523@gmail.com>
2018-09-22 09:45:58 +0800

    Reinforce finDataScraper.py

--- finDataScraper.py
+++ finDataScraper.py
@@ -1,27 +1,68 @@
+import time
+from random import randint
 import json
 import requests
 from bs4 import BeautifulSoup
 
+def announce(message, wait=0, skip=0):
+  print(message, end=' (waiting for {} seconds)'.format(wait) if wait is not 0 else '')
+  while wait > 0:
+      print('.', end='', flush=True)
+      time.sleep(1)
+      wait -= 1
+  print('\n' * skip if skip is not 0 else '')
+
+def report(message):
+  print(message)
+  time.sleep(0.2)
+
 class FinDataScraper:
-  mode = {}
   region = 'HK'
-  symbol = ''
-  site = ''
+  symbols = []
+  site = 'http://basic.10jqka.com.cn'
 
   resonance = None
   position = None
   cashFlow = None
   financials = []
 
-  def __init__(self, symbol, verbose=False):
-    self.mode['verbose'] = verbose
-    self.symbol = symbol if len(symbol) == 4 else '0'*(4-len(symbol))+symbol
-    self.site = 'http://basic.10jqka.com.cn/{}{}/finance.html'.format(self.region, self.symbol)
-    print('Symbol: {}'.format(self.symbol))
-    print('Site: {}'.format(self.site))
-  
-  def get_all_statements(self):
-    print('Setting headers...')
+  def __init__(self, symbol, fromSymbol=None):
+    print('\n'*20)
+    if symbol == 'ALL':
+      announce("REMINDING: You're conducting a COMPLETE financial data search", wait=7, skip=7)
+      def retrieve_all_symbols():           # Format: [symbol, companyName]
+        report('Retrieving all possible symbols and companyNames from HKEX...\n')
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8,zh-TW;q=0.7,zh;q=0.6,zh-CN;q=0.5'
+        }
+        site = 'http://www.hkexnews.hk/listedco/listconews/advancedsearch/stocklist_active_main_c.htm'
+
+        session = requests.session()
+        response = session.get(site, headers=headers)
+        bs = BeautifulSoup(response.content, 'lxml')
+        
+        report('Data retrieved. Further processing...\n')
+        result = bs.select("[class^=TableContentStyle]")
+        result = list(filter(lambda x: x.contents[0].get_text()[0] == '0' and x.contents[0].get_text()[1] in ['0', '1', '2', '3'], result))
+        result = list(map(lambda x: [x.contents[0].get_text(), x.contents[1].get_text()], result))
+        announce('Data are already formatted in form of [symbol, companyName]', skip=7)
+        return result
+      self.symbols = retrieve_all_symbols()
+      if fromSymbol:
+        for i, unit in enumerate(self.symbols):
+          if unit[0].endswith(fromSymbol):
+            self.symbols = self.symbols[i:]
+            print('Starting from {} {}.'.format(unit[1], unit[0]))
+            break
+    else:
+      self.symbols = [symbol if len(symbol) == 5 else '0'*(5-len(symbol))+symbol]
+    report('Task:\n\tTarget {} from {}.'.format(symbol, self.site))
+
+  def get_all_statements(self, symbol, retryMax):
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
@@ -30,20 +71,30 @@ class FinDataScraper:
         'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8,zh-TW;q=0.7,zh;q=0.6,zh-CN;q=0.5'
     }
 
-    print('Retrieving the site...')
     session = requests.session()
-    response = session.get(self.site, headers=headers)
 
-    print('Retrieving the data...')
+    for i in range(retryMax):
+      try:
+        response = session.get('{}/{}{}/finance.html'.format(self.site, self.region, symbol), headers=headers)
+        break
+      except:
+        announce('Retrying again', wait=(i*10+randint(0,4)))
+        pass
+
+    report('Online source retrieved. Now getting into the data...')
     bs = BeautifulSoup(response.content, 'lxml')
-    self.resonance = json.loads(bs.find(id='benefit').get_text())
-    print('利潤表 retrieved.')
-    self.position = json.loads(bs.find(id='debt').get_text())
-    print('資產負債表 retrieved.')
-    self.cashFlow = json.loads(bs.find(id='cash').get_text())
-    print('現金流量表 retrieved.')
+    if bs.find(id='benefit'):
+      self.resonance = json.loads(bs.find(id='benefit').get_text())
+      report('利潤表 retrieved.')
+      self.position = json.loads(bs.find(id='debt').get_text())
+      report('資產負債表 retrieved.')
+      self.cashFlow = json.loads(bs.find(id='cash').get_text())
+      report('現金流量表 retrieved.')
+      return True
+    else:
+      return None
 
-  def sort_financials(self, verbose=False):
+  def sort_financials(self):
     # Helper Functions
     def make_hundred_millions(value, originalUnit):
       if value is not '':
@@ -55,21 +106,24 @@ class FinDataScraper:
       else:
         return None
     
+    # Reset financials
+    self.financials = []
+    
     # Get Periods Ready
-    print('self.resonance is alright.')
+    report('Retrieving periods...')
     periods = self.resonance["report"][0]
-    print('Periods retrieved.')
 
     # Get Statements Clear
-    print('Processing 利潤表...')
+    report('Processing all statements...')
+    # Processing 利潤表
     self.resonance['title'] = self.resonance['title'][1:]
     self.resonance['report'] = self.resonance['report'][1:]
 
-    print('Processing 資產負債表...')
+    # Processing 資產負債表
     self.position['title'] = self.position['title'][1:]
     self.position['report'] = self.position['report'][1:]
     
-    print('Processing 現金流量表...')
+    # Processing 現金流量表
     self.cashFlow['title'] = self.cashFlow['title'][1:]
     self.cashFlow['report'] = self.cashFlow['report'][1:]
 
@@ -89,7 +143,6 @@ class FinDataScraper:
       }
       
       # Mark Currency
-      print('Learning currency...')
       sampleUnit = self.resonance['title'][0][1]
       if sampleUnit.endswith('港元'):
         financial['currency'] = '億HKD'
@@ -151,47 +204,111 @@ class FinDataScraper:
         }
         if name in mapForCashFlow:
           value = make_hundred_millions(self.cashFlow['report'][j][i], originalUnit)
-          print(name)
           financial['cashFlow'][mapForCashFlow[name]] = value
 
-      if self.mode['verbose']:
-        print(financial)
+      report('Data of {} loaded.'.format(financial['year']))
       self.financials.append(financial)
 
-  def upload(self, apiUrl, token, mode):
-    print('Starting posting financials to Company {}{}...'.format(self.region, self.symbol))
-
-    print('Setting headers...')
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token {}'.format(token)
-    }
-
-    session = requests.session()
-
-    api = '/companies/{}/financials'.format(self.symbol.lstrip('0'))
-
-    for financial in self.financials:
-      data = {'financial': financial }
-      json_string = json.dumps(data)
-
-      if self.mode['verbose']:
-        print('{} financial {} of Company {}'.format(('Posting' if mode == 'post' else '') + ('Updating' if mode == 'update' else ''), financial['year'], self.symbol))
-      
-      if mode == 'post':  
-        response = session.post('{}{}'.format(apiUrl, api), headers=headers, data=json_string)
-      elif mode == 'update':
-        response = session.put('{}{}/{}'.format(apiUrl, api, financial['year']), headers=headers, data=json_string)
+  def process(self, apiUrl, token, retryMax):
+    def log(response):
       bs = BeautifulSoup(response.content, 'lxml')
-
       json_string = bs.get_text()
       data = json.loads(json_string) if '{' in json_string and '}' in json_string else None
       if data:
         if 'errors' in data:
-          print('Response {} of Company {}: '.format(financial['year'][:4], self.symbol), data['errors'])
-        elif self.mode['verbose']:
-          print('Response: ', data)
+          print('Response: {}'.format(data['errors']))
+        else:
+          print('Success.')
+        return data
       else:
         print('Error: {}'.format(json_string))
+        return json_string
+      
     
-    print('Uploads complete.')
+    announce('Start!', skip=7)
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token {}'.format(token)
+    }
+    session = requests.session()
+
+    for symbol, companyName in self.symbols:
+      apis = {
+        'company': {
+          'get': '/companies/{}'.format(symbol.lstrip('0')),
+          'post': '/companies'
+        },
+        'financial': '/companies/{}/financials'.format(symbol.lstrip('0'))
+      }
+
+      # Check if the company is listed in the source site
+      announce('{}: Getting resonance, position, cashFlow statements'.format(companyName), wait=(7+randint(0,4)))
+      if self.get_all_statements(symbol[1:], retryMax):
+        # Ensure the company has been created.
+        for i in range(retryMax):
+          try:
+            company = session.get('{}{}'.format(apiUrl, apis['company']['get']), headers=headers)
+            break
+          except:
+            announce('Retrying again', wait=(i*10+randint(0,4)))
+            pass
+        
+        if log(company) == 'Unauthorized':
+          announce('Creating company {} {}...'.format(companyName, symbol), wait=3)
+          data = {
+              'company': {
+                  'symbol': symbol.lstrip('0'),
+                  'name': companyName
+              }
+          }
+          json_company = json.dumps(data)
+          response = session.post('{}{}'.format(apiUrl, apis['company']['post']), headers=headers, data=json_company)
+          log(response)
+          report('Company created.')
+        else:
+          report('Company existed.')
+
+        # Sort self.resonance, self.position and self.cashFlow into self.financials
+        self.sort_financials()
+
+        # Check the original recorded financials
+        for i in range(retryMax):
+          try:
+            response = session.get('{}{}'.format(apiUrl, apis['financial']), headers=headers)
+            break
+          except:
+            announce('Retrying again', wait=(i*10+randint(0,4)))
+            pass
+        originalFinancials = log(response)['financials']
+        originalFinancials = list(map(lambda financial: financial['year'], originalFinancials))
+
+        # Upload self.financials   
+        for financial in self.financials:
+          data = { 'financial': financial }
+          json_string = json.dumps(data)
+
+          # Check if the financial of the year already exists
+          existed = financial['year'] in originalFinancials
+          report('{}: {} financial {}'.format(companyName, 'Updating' if existed else 'Posting', financial['year']))
+          if existed:
+            for i in range(retryMax):
+              try:
+                response = session.put('{}{}/{}'.format(apiUrl, apis['financial'], financial['year']), headers=headers, data=json_string)
+                break
+              except:
+                announce('Retrying again', wait=(i*10+randint(0,4)))
+                pass
+          else:
+            for i in range(retryMax):
+              try:
+                response = session.post('{}{}'.format(apiUrl, apis['financial']), headers=headers, data=json_string)
+                break
+              except:
+                announce('Retrying again', wait=(i*10+randint(0,4)))
+                pass
+          log(response)
+        announce('Uploads for {} completed.'.format(companyName), skip=3)
+      else:
+        report('Company {}{} is not listed at {}'.format(self.region, symbol, self.site))
+        announce('Skip', skip=3)
+    print('Processing Completed.')
--- sodium.py
+++ sodium.py
@@ -2,29 +2,14 @@ from sys import argv
 from finDataScraper import FinDataScraper
 from finReportHandler import FinReportHandler
 
-if __name__ == '__main__':
-  def get_param(name, options):
-    prefix = 3 + len(name)
-    provided = [opt[prefix:] for opt in options if '--{}'.format(name) in opt]
-    return provided[0] if provided else None
-
-  options = [arg for arg in argv if arg.startswith('-')]
-  argv = [arg for arg in argv if arg not in options]
-
-  default = {
-    'retryMax': 3,
-    'downloadDirectory': 'downloaded'
-  }
-
-  if 'help' in argv or len(argv) is 1:
-    description = """
+description = """
 NAME
       sodium - a helper to automate the visualisation of financial data of listed companies.
 
 SYNOPSIS:
       python sodium.py help
-      python sodium.py [ -S ] [ -t | -T ] [-n] [ -m ] [ -C ] [ --directory=DIRECTORY ] [ --retry=MAX ] SYMBOL
-      python sodium.py scrape [--apiUrl] [--token] [ -p | -u ] SYMBOL
+      python sodium.py [ -S ] [ -t | -T ] [ -n ] [ -m ] [ -C ] [ --directory=DIRECTORY ] [ --retry=MAX ] SYMBOL
+      python sodium.py scrape --apiUrl=API_URL --token=TOKEN [ --fromSymbol=SYMBOL ] [ --retry=MAX ] [ ALL | SYMBOL ]
 
 OPTIONS:
                     (Default mode)
@@ -35,52 +20,60 @@ OPTIONS:
       -m            Merge downloaded financial reports for further studies.
       -C            Clean up downloaded financial reports.
 
-                    (Scraping mode)
-      -p            Allow sending POST requests. (Params 'apiUrl' and 'token' should be provided.)
-      -u            Allow sending PUT requests. (Params 'apiUrl' and 'token' should be provided.)
-
 PARAMS:
+                    (Common)
+      --retryMax    Number of retries made when failed to download the pdf from HKEX. Default: 3
+
                     (Default mode)
       --directory   The download directory. Default: downloaded
-      --retry       Number of retries made when failed to download the pdf from HKEX. Default: 3
 
                     (Scraping mode)
       --apiUrl      API url for sending requests
       --token       Authorization token
+      --fromSymbol  Starting symbol
 """
+
+if __name__ == '__main__':
+  def get_param(name, options):
+    prefix = 3 + len(name)
+    provided = [opt[prefix:] for opt in options if '--{}'.format(name) in opt]
+    return provided[0] if provided else None
+
+  options = [arg for arg in argv if arg.startswith('-')]
+  argv = [arg for arg in argv if arg not in options]
+
+  default = {
+    'retryMax': 7,
+    'downloadDirectory': 'downloaded'
+  }
+
+  if 'help' in argv or len(argv) is 1:
     print(description)
   elif 'scrape' in argv:
+    apiUrl = get_param('apiUrl', options)
+    token = get_param('token', options)
+    fromSymbol = get_param('fromSymbol', options)
+    retryMax = get_param('retryMax', options)
+
+    retryMax = int(retryMax if retryMax is not None and retryMax.isdigit() else default['retryMax'])
+
     if len(argv) < 3:
       print(description, '\n'*2)
       print('WARNING:')
-      print('\tSymbol should be provided.')
+      print('\tTarget should be provided.', '\n'*2)
       exit()
     else:
-      symbol = argv[2]
+      target = argv[2]
 
-    setVerbose = '-v' in options
-    allowPost = '-p' in options
-    allowUpdate = '-u' in options
-    if allowPost or allowUpdate:
-      apiUrl = get_param('apiUrl', options)
-      token = get_param('token', options)
-      if apiUrl in [None, ''] or token in [None, '']:
-        print(description, '\n'*2)
-        print('WARNING:')
-        print('\tApiUrl and token should be provided for sending POST requests.')
-        exit()
+    if apiUrl in [None, ''] or token in [None, '']:
+      print(description, '\n'*2)
+      print('WARNING:')
+      print('\tApiUrl and token should be provided for sending POST requests.', '\n'*2)
+      exit()
 
-    scraper = FinDataScraper(symbol, setVerbose)
+    scraper = FinDataScraper(target, fromSymbol)
 
-    scraper.get_all_statements()
-
-    scraper.sort_financials()
-
-    if apiUrl not in [None, ''] and token not in [None, '']:
-      if allowUpdate:
-        scraper.upload(apiUrl, token, 'update')
-      elif allowPost:
-        scraper.upload(apiUrl, token, 'post')
+    scraper.process(apiUrl, token, retryMax)
   else:
     symbol = argv[1]
     skipDownload = '-S' in options
@@ -89,7 +82,7 @@ PARAMS:
     needMergeFiles = '-m' in options
     needCleanUp = '-C' in options
     downloadDirectory = get_param('directory', options)
-    retryMax = get_param('retry', options)
+    retryMax = get_param('retryMax', options)
 
     downloadDirectory = downloadDirectory if downloadDirectory not in [None, ''] else default['downloadDirectory']
     retryMax = int(retryMax if retryMax is not None and retryMax.isdigit() else default['retryMax'])
